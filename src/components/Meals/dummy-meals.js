b2f9cc64874a8066e250e3ab698bcd891898fb6e
AVINASH.S.A <103623032+AvinashAravindan@users.noreply.github.com>
2024-03-13 09:35:00 +0530

    Add files via upload

---
+++ src/components/Meals/dummy-meals.js
@@ -0,0 +1,28 @@
+const DUMMY_MEALS = [
+  {
+    id: "m1",
+    name: "Briyani",
+    description: "A savoury spiced rice pilaf",
+    price: 300,
+  },
+  {
+    id: "m2",
+    name: "Vada",
+    description: "A savoury fried snacks",
+    price: 40,
+  },
+  {
+    id: "m3",
+    name: "Dosa",
+    description: "Say yes to Dosa",
+    price: 120,
+  },
+  {
+    id: "m4",
+    name: "Samosa",
+    description: "Simple samosa",
+    price: 50,
+  },
+];
+
+export default DUMMY_MEALS;
